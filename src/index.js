9c8b8baf50c4899b6febf9f1fabbcb9e659fea67
Timothy Keaveny <timothykeaveny@Timothys-MacBook-Pro.local>
2018-06-12 22:30:39 -0400

    Install Redux and create preemtive React/Redux components

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+// createStore is where we store all the data for the app
+import { createStore } from 'redux';
+// chat (the reducer) is the App's data
+import chat from './reducers';
+// The store var calls "createStore" and passes our data for storage
+const store = createStore(chat)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Our app's state will always be "read only" when using redux
+// To alter the state, we use "actions"
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
